8d11187d666f1b969bf85e95eac447a955b3e8e2
Jay Nguyen <jay@Jays-iMac.local>
2022-02-11 19:19:59 -0800

    file setup

---
+++ server.js
@@ -0,0 +1,31 @@
+// EXPRESS SERVER SETUP
+const express = require('express');
+const app = express();
+
+// Express comes with a method called .use() that allows us to execute some logic for all routes. In order to signal that the middleware is complete we call next().
+app.use((req, res, next) => {
+	console.log('EXECUTING FOR ALL ROUTES');
+	next();
+});
+
+// IMPORT AND INITIALIZE DOTENV PACKAGE
+// This tool will automatically look for a file called .env on the same level as server.js. Must create .env file. Be sure to update the .gitignore file to include .env
+require('dotenv').config();
+
+// POSTMAN SETUP
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// AFTER INSTALLING MORGAN FROM NODE, IMPORT MORGAN LOGGER--AN EXPRESS MIDDLEWARE WITH THE PURPOSE OF DEBUGGING AND BETTER LOGGING FOR CRUD REQUESTS
+// TERMINAL/NODE.JS WILL DISPLAY :method :url :status :response-time ms :res[content-length]
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// IMPORT CONTROLLER FILE
+
+// PORT STUFF
+// ONCE SERVER.JS IS LIVE, 'SHOW ROUTES' WILL DISPLAY AT http://localhost:4000
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+	console.log('👬 HubSpot server.js is listening on Port 4000');
+});
